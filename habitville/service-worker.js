d58f228efe46857ca25743762270b9b3b6f5bf95
mike <mlflabs@hotmail.com>
2020-02-18 15:43:00 +0800

    updated habitville client

--- habitville/service-worker.js
+++ habitville/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4816cc930464e89f69e1879772bc90b3.js"
+  "/habitville/precache-manifest.2d4b54ca52510ca5fe6cd1f05171c945.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/habitville/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
